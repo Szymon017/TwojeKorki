8d3bc895da1f29c074c260d27c1e3875ea865ac2
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2022-10-10 14:34:37 +0200

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+const PORT = Number(process.env.PORT) || 5001;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(5000, () => {
-    console.log(`Server runnin' on port 5000`);
+app.listen(PORT, () => {
+    console.log(`Server runnin' on port ${PORT}`);
 })
 
 
