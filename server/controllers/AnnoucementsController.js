f7659a9250d2add0669aed560277e26c2a604c59
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2022-10-29 00:35:08 +0200

    Annoucement all routes working - beta

--- server/controllers/AnnoucementsController.js
+++ server/controllers/AnnoucementsController.js
@@ -40,26 +40,53 @@ const getAnnoucementById = async (req, res) => {
 }
 
 const addNewAnnoucement = async (req, res) => {
+    const {title, description, author, price, category, option, date} = req.body;
+
     try {
-
+        const newAnnoucement = await Annoucement.create({
+            title, description, author, price, category, option, date
+        })
+        res.status(200).json({
+            status: 'Successfully created an annoucement',
+            data: newAnnoucement
+        });
     } catch (error) {
-
-    }
-}
-
-const updateAnnoucement = async (req, res) => {
-    try {
-
-    } catch (error) {
-
+        res.send(500).json({
+            status: 'Failed to create an annoucement',
+            message: error
+        })
     }
 }
 
 const deleteAnnoucement = async (req, res) => {
     try {
-
+        const result = await Annoucement.findOneAndDelete({_id: req.params.id})
+        res.status(201).json({
+            status: 'Successfully deleted annoucement',
+            data: null
+          });
     } catch (error) {
+        res.status(409).json({
+            status: 'Failed to delete an annoucement',
+            message: error
+        });
+    }
+}
 
+const updateAnnoucement = async (req, res) => {
+    try {
+        const newAnnoucement = await Annoucement.findByIdAndUpdate(req.params.id, req.body, {
+            new:true
+        });
+        res.status(200).json({
+            status: 'Succesfully updated an annoucement',
+            data: newAnnoucement
+          })
+    } catch (error) {
+        res.status(500).json({
+            status: 'Failed to update an annoucement',
+            message: error.message
+          });
     }
 }
 
--- server/routes/Annoucements.js
+++ server/routes/Annoucements.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {getAllAnnoucements, getAnnoucementById, addNewAnnoucement, updateAnnoucement, deleteAnnoucement} from '../controllers/AnnoucementsController.js';
+import annoucementValidator from "../middleware/validation/annoucementValidator.js";
 const router = express.Router();
 
 router.get('/', getAllAnnoucements);
 router.get('/:id', getAnnoucementById);
-router.post('/', addNewAnnoucement);
+router.post('/', annoucementValidator,addNewAnnoucement);
 router.patch('/:id', updateAnnoucement);
 router.delete('/:id', deleteAnnoucement);
 
