626989caab4c5540dcbbb3ac89505594fd4f4d20
Mateusz Izdebski <mati2k51@gmail.com>
2022-10-29 01:03:39 +0200

    Dodanie nowej rejestracji loginu navbaru, pomoc bezkoder

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,138 @@
-import React from 'react'
-import { DefaultLayout } from './layout/DefaultLayout';
-import { Dashboard } from './pages/dashboard/Dashboard.page';
-import './App.css';
-import {Entry} from './pages/entry/Entry.page';
-import { AnnouncementLists } from './pages/announcement-list/AnnouncementList.page';
-import {AddAnnoucement} from './pages/new-announcement/AddAnnouncement.page';
-import { Registration } from './pages/registration/Registration.page';
+import React, { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthService from "./services/auth.service";
 
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+import Home from "./components/home.component";
+import Profile from "./components/profile.component";
+import BoardUser from "./components/board-user.component";
+import BoardModerator from "./components/board-moderator.component";
+import BoardAdmin from "./components/board-admin.component";
 
-function App() {
-  return (
-    <div className='App'>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
 
-      <Router>
-        <Routes>
-            <Route exact path="/" element={<Entry />} />
-            <Route exact path="/registration" element={<Registration />} />
-            <Route path="/dashboard" element={<DefaultLayout><Dashboard/></DefaultLayout>} />
-            <Route path="/announcements" element={<DefaultLayout><AnnouncementLists/></DefaultLayout>} />
-            <Route path="/add" element={<DefaultLayout><AddAnnoucement/></DefaultLayout>} />
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
 
-        
-        
-        </Routes>
-      </Router>
-    
-    </div>
-  );
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
+    }
+  }
+
+  logOut() {
+    AuthService.logout();
+    this.setState({
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    });
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+            TwojeKorki
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+            
+
+            {showModeratorBoard && (
+              <li className="nav-item">
+                <Link to={"/mod"} className="nav-link">
+                  Moderator Board
+                </Link>
+              </li>
+            )}
+
+            {showAdminBoard && (
+              <li className="nav-item">
+                <Link to={"/admin"} className="nav-link">
+                  Admin Board
+                </Link>
+              </li>
+            )}
+
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
+          </div>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/user" element={<BoardUser />} />
+            <Route path="/mod" element={<BoardModerator />} />
+            <Route path="/admin" element={<BoardAdmin />} />
+          </Routes>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
