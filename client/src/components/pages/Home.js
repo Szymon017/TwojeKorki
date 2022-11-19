c84d484395e42c2f555312a0d772ca4b07147c6c
Szymon Kacprzyk <szymon.kacprzyk5@gmail.com>
2022-11-20 00:14:00 +0100

    Quick fix

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,30 @@
-import {Link} from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import './style.css';
 export default function Home() {
-    return <>
-    <div class="text-justify">
-
-        <h1 class="text-justify">Witaj na stronie TwojeKorki.pl</h1>
-        <Link to="/register"> Zarejestruj się aby w pełni korzystać z serwisu!</Link>
-        <p>Masz już konto? <Link to="/login">Zaloguj się tutaj</Link></p>
-    </div>
-    </> 
+  return (
+    <>
+      <Container className='homeLogo'>
+        <Row>
+            <Col className='homeLogo'>
+                <h1>Witaj na stronie</h1>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            <h1 className='homeLogo2'>TwojeKorki</h1>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            <h4 className='homeLogo2'>Potrzebujesz pomocy z zadaniem? Nie rozumiesz matematyki?</h4>
+            <h4 className='homeLogo2'>Trafiłeś w idealne miejsce! </h4>
+            <h4 className='homeLogo2'> <Link to="/register"> Zarejestruj się aby w pełni korzystać z serwisu!</Link></h4>
+            <h4 className='homeLogo2'>Masz już konto? <Link to="/login">Zaloguj się tutaj</Link></h4>
+            
+            </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
--- client/src/components/pages/UserProfile/UserProfile.js
+++ client/src/components/pages/UserProfile/UserProfile.js
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import img from '../../../assets/images/a1.jpg';
-import './style.css';
-import { getCurrentUser } from '../../../service/userDataService';
-import { getUserAnnoucements } from '../../../service/userService';
+import React, { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import img from "../../../assets/images/a1.jpg";
+import "./style.css";
+import { getCurrentUser } from "../../../service/userDataService";
+import { getUserAnnoucements } from "../../../service/userService";
 
 export default function UserProfile() {
   const [user, setUser] = useState(getCurrentUser());
   const [annoucements, setAnnoucements] = useState();
 
+  const getAnn = async () => {
+    const result = await getUserAnnoucements(user._id);
+    setAnnoucements(result.data);
+  };
 
-  const getAnn = async () =>{
-    const result = await getUserAnnoucements(user._id)
-    setAnnoucements(result.data)
-  }
-
-
+  useEffect(() => {
+    getAnn();
+  }, [user]);
 
   return (
     <div>
@@ -26,33 +27,43 @@ export default function UserProfile() {
       <Container className=" my-5 justify-content-center">
         <Row>
           {/* Leftbar*/}
-          <Col md={4} className='profileLeftPanel'>
+          <Col md={4} className="profileLeftPanel">
             <Row>
-              <img src={img} className='profilePhoto'></img>
+              <img src={img} className="profilePhoto"></img>
             </Row>
             <Row className="my-3">
-                <h2>{user.firstName+" "+user.lastName}</h2>
-                <h5>{user.email}</h5>
-                <h5>{user.telephone}</h5>
-                <button>Edytuj dane</button>
+              <h2>{user.firstName + " " + user.lastName}</h2>
+              <h5>{user.email}</h5>
+              <h5>{user.telephone}</h5>
+              <button className="btn btn-dark">Edytuj dane</button>
             </Row>
           </Col>
           {/* middle + rightbar*/}
-          <Col md={7} className='profileMiddlePanel'>
+          <Col md={7} className="profileMiddlePanel">
             <Row>
-              <h2>Uzytkownik nie zamieścił o sobie zadnych informacji</h2>
-              <button onClick={getAnn}>Zobacz ogłoszenia użytkownika</button>
-              {annoucements?(
-
+              <Col>
+                <button onClick={getAnn} className="btn btn-dark">
+                  Zobacz ogłoszenia użytkownika
+                </button>
+              </Col>
+              <Col>
+                <button onClick={getAnn} className="btn btn-dark">
+                  Zobacz opinie o użytkowniku
+                </button>
+              </Col>
+            </Row>
+              {annoucements ? (
                 annoucements.map((ann) => (
-                  <p>{ann.title + " " + ann.category + " " + ann.option}</p>
-                  ))
-                  ):""}
-            </Row>
-
-            <Row>
-              
-            </Row>
+                  <Row>
+                    <Col><p className='annoucementTile'>{ann.title}</p></Col>
+                    <Col><p className='annoucementTile'>{ann.category}</p></Col>
+                    <Col><p className='annoucementTile'>{ann.option}</p></Col>
+                  </Row>
+                ))
+              ) : (
+                <h2>Brak dostepnych informacji</h2>
+              )}
+            
 
             <Row></Row>
           </Col>
