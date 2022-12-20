3c34c22da49a70a3bd93dd3307835d28db144477
Mateusz Izdebski <mati2k51@gmail.com>
2022-12-21 00:11:51 +0100

    Register i login oprawiony plus pole edycji ogloszenia dodane

--- client/src/components/pages/UserProfile/OneAnnInProf.js
+++ client/src/components/pages/UserProfile/OneAnnInProf.js
@@ -10,70 +10,66 @@ import { deleteAnnoucement } from '../../../service/announcementService';
 
 export default function OneAnnInProf(props) {
   const { announce } = props;
-  useEffect(()=>{
-  })
+  useEffect(() => {});
 
   const deleteAnnounce = (id) => {
     deleteAnnoucement(id);
-    window.location.assign("/userProfile");
-  }
+    window.location.assign('/userProfile');
+  };
   return (
- 
-      <Row className="g-0 singleAnnouce">
-        <Col md={2}>
-          <Row className="g-0 ">
-            <Col md={12} className="d-flex justify-content-center">
+    <Row className="g-0 singleAnnouce">
+      <Col md={2}>
+        <Row className="g-0 ">
+          <Col md={12} className="d-flex justify-content-center">
+            <Link to={`/announcement/${announce.title}`}>
+              <img
+                className="img-fluid rounded-start"
+                src={img}
+                alt={announce.option}
+              ></img>
+            </Link>
+          </Col>
+        </Row>
+      </Col>
+      <Col md={10}>
+        <Card.Body>
+          <Row className="g-0">
+            <Col md={12}>
+              <Card.Title>{announce.title}</Card.Title>
+            </Col>
+            <Col md={12}>
+              <Card.Text>{announce.description}</Card.Text>
+            </Col>
+            <Col md={6} className=" fw-bold">
+              <Card.Text>{announce.location} </Card.Text>
+            </Col>
+            <Col md={6} className="text-end fs-5 fw-bold ">
+              {' '}
+              <Card.Text> {announce.price} zł/godz</Card.Text>
+            </Col>
+            <Col className="text-end">
               <Link to={`/announcement/${announce.title}`}>
-                <img
-                  className="img-fluid rounded-start"
-                  src={img}
-                  alt={announce.option}
-                ></img>
+                <Button variant="warning">Otwórz</Button>
+              </Link>{' '}
+              <Link to={'/annoucements/edit'}>
+                <Button variant="warning">
+                  Edytuj <i class="bi bi-pencil"></i>
+                </Button>
+              </Link>{' '}
+              <Link to={`/userProfile`}>
+                <Button
+                  variant="warning"
+                  onClick={() => {
+                    deleteAnnounce(announce._id);
+                  }}
+                >
+                  Usuń <i class="bi bi-trash"></i>
+                </Button>
               </Link>
             </Col>
           </Row>
-        </Col>
-        <Col md={10}>
-          <Card.Body>
-            <Row className="g-0">
-              <Col md={12}>
-                <Card.Title>{announce.title}</Card.Title>
-              </Col>
-              <Col md={12}>
-                <Card.Text>{announce.description}</Card.Text>
-              </Col>
-              <Col md={6} className=" fw-bold">
-                <Card.Text>{announce.location} </Card.Text>
-              </Col>
-              <Col md={6} className="text-end fs-5 fw-bold ">
-                {' '}
-                <Card.Text> {announce.price} zł/godz</Card.Text>
-              </Col>
-              <Col className="text-end" >
-                <Link to={`/announcement/${announce.title}`}>
-                  <Button variant="warning">
-                    Otwórz
-                  </Button>
-                </Link>
-              
-                {' '}
-                <Link to={'/'}>
-                  <Button variant="warning">
-                    Edytuj <i class="bi bi-pencil"></i>
-                  </Button>
-                </Link>
-              
-                {' '}
-                <Link to={`/userProfile`}>
-                  <Button variant="warning" onClick={() => {deleteAnnounce(announce._id)}}>
-                    Usuń <i class="bi bi-trash"></i>
-                  </Button>
-                </Link>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Col>
-      </Row>
-   
+        </Card.Body>
+      </Col>
+    </Row>
   );
 }
